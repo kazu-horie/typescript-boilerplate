e747a8d09bb14c7614060842c633a471256a1048
Kazuki Horie <kazukihorie.jp@gmail.com>
2020-06-15 01:24:32 +0900

    Install ESLint and Prettier

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-function greeter(person: string) {
-  return "Hello, " + person;
-}
-
-let user = "Jane User";
+const greeter = (person: string): string => {
+    return `Hello, ${person}`;
+  },
+  user = 'Jane User';
 
 document.body.textContent = greeter(user);
 
