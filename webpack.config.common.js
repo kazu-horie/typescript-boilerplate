885d313f99f0006adcc7c3df94c6d237071e4ec3
Kazuki Horie <kazukihorie.jp@gmail.com>
2020-07-26 02:48:59 +0900

    Use core-js

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -3,7 +3,7 @@ const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: ['@babel/polyfill', './src/index.ts'],
+  entry: './src/index.ts',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
